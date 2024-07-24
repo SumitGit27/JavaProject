96212932adeefdc2d6cd1b5c918c63f758ac6a09
SumitGit27 <rdj05424@gmail.com>
2024-07-24 18:02:54 +0530

    Addedprogram

---
+++ src/javaPrograms/AdditionEx1.java
@@ -0,0 +1,17 @@
+package javaPrograms;
+
+public class AdditionEx1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program Starts");
+		AdditionEx1 obj = new AdditionEx1();
+		obj.addition(80, 80);
+		System.out.println("Program Ends");
+	}
+	
+	public void addition(int a, int b) {
+		int sum = a+b;
+		System.out.println("Addition = "+sum);
+	}
+}
---
+++ src/javaPrograms/SubstrctionEx2.java
@@ -0,0 +1,17 @@
+package javaPrograms;
+
+public class SubstrctionEx2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program Starts");
+		SubstrctionEx2 obj = new SubstrctionEx2();
+		obj.addition(100, 80);
+		System.out.println("Program Ends");
+	}
+	
+	public void addition(int a, int b) {
+		int sum = a+b;
+		System.out.println("Substrction = "+sum);
+	}
+}
